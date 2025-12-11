cac6bfb13a0125fb277c15f328c60eedbe6e23e6
nasirhasanovic <hasanovicnasir@gmail.com>
2025-12-11 19:02:00 +0100

    spacing constants

---
+++ RentDar/Core/Design/Theme/AppSpacing.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+enum AppSpacing {
+    static let xs: CGFloat = 4
+    static let sm: CGFloat = 8
+    static let md: CGFloat = 12
+    static let lg: CGFloat = 16
+    static let xl: CGFloat = 20
+    static let xxl: CGFloat = 24
+    static let xxxl: CGFloat = 32
+    static let screenPadding: CGFloat = 24
+    static let buttonHeight: CGFloat = 52
+    static let cornerRadius: CGFloat = 12
+    static let cornerRadiusSmall: CGFloat = 8
+    static let inputHeight: CGFloat = 52
+}
